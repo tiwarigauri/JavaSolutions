3206b4b0b613aeff66c5f7ce8ff69b69a8bd10e5
tiwarigauri <gauri.uiet@gmail.com>
2023-10-24 19:56:32 +0530

    add given three numbers

---
+++ addThreeNumbers.java
@@ -0,0 +1,9 @@
+public class addThreeNumbers {
+    public static void main(String[] args){
+        int x=10;
+        int y=20;
+        int z=30;
+        int sum= x+y+z;
+        System.out.println("Sum of the three numbers is: " +sum);
+    }
+}
